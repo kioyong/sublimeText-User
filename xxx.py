7160d13a6e3924ef3670714c3bca68f359a42bd8
mko684 <yong.a.liang@accenture.com>
2017-09-19 23:29:09 +0800

    add format ids demo gif

--- xxx.py
+++ xxx.py
@@ -5,3 +5,11 @@ class xxxCommand(sublime_plugin.WindowCommand):
         window = self.window
         window.run_command("select_all")
         window.run_command("split_selection_into_lines")
+        window.run_command("move_to",{"extend":False,"to":"bol"})
+        window.run_command("insert", {"characters": "\""})
+        window.run_command("move_to",{"extend":False,"to":"bol"})
+        window.run_command("move_to",{"extend":False,"to":"eol"})
+        window.run_command("insert", {"characters": "\","})
+        window.run_command("right_delete")
+        window.run_command("move_to",{"extend":False,"to":"eol"})
+        window.run_command("left_delete")
